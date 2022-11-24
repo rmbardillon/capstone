02b593b96ace195b0fafc88b17c22934436235c5
Romeo Jr Montealegre Bardillon <rmbardillonjr@iskolarngbayan.pyup.edu.ph>
2022-11-24 14:59:41 +0800

    Added footer html

--- administrator/assets/js/function.js
+++ administrator/assets/js/function.js
@@ -6,6 +6,7 @@ $(document).ready( function () {
     $('#pwdDataTable').DataTable();
     $('#spDataTable').DataTable();
     $('#scDataTable').DataTable();
+    $('#announcements').DataTable();
 });
 
 // Profile Picture
