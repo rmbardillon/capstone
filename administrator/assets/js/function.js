12dd044859297a727d540903810dd717681eddfa
Romeo Jr Montealegre Bardillon <rmbardillonjr@iskolarngbayan.pyup.edu.ph>
2022-11-24 22:03:34 +0800

    added export table data using datatables

--- administrator/assets/js/function.js
+++ administrator/assets/js/function.js
@@ -1,12 +1,53 @@
 $(document).ready( function () {
-    $('#mainAdministrators').DataTable();
-    $('#pwdAdministrators').DataTable();
-    $('#soloparentAdministrators').DataTable();
-    $('#srCitizenAdministrators').DataTable();
-    $('#pwdDataTable').DataTable();
-    $('#spDataTable').DataTable();
-    $('#scDataTable').DataTable();
-    $('#announcements').DataTable();
+    var mainAdministratorsTable = $('#mainAdministrators').DataTable({
+        
+        buttons:['copy', 'csv', 'excel', 'pdf', 'print']
+        
+    });
+    var pwdAdministratorsTable = $('#pwdAdministrators').DataTable({
+        
+        buttons:['copy', 'csv', 'excel', 'pdf', 'print']
+        
+    });
+    var soloparentAdministratorsTable = $('#soloparentAdministrators').DataTable({
+        
+        buttons:['copy', 'csv', 'excel', 'pdf', 'print']
+        
+    });
+    var srCitizenAdministratorsTable = $('#srCitizenAdministrators').DataTable({
+        
+        buttons:['copy', 'csv', 'excel', 'pdf', 'print']
+        
+    });
+    var pwdDataTable = $('#pwdDataTable').DataTable({
+        
+        buttons:['copy', 'csv', 'excel', 'pdf', 'print']
+        
+    });
+    var spDataTable = $('#spDataTable').DataTable({
+        
+        buttons:['copy', 'csv', 'excel', 'pdf', 'print']
+        
+    });
+    var scDataTable = $('#scDataTable').DataTable({
+        
+        buttons:['copy', 'csv', 'excel', 'pdf', 'print']
+        
+    });
+    var announcementsTable = $('#announcements').DataTable({
+        
+        buttons:['copy', 'csv', 'excel', 'pdf', 'print']
+        
+    });
+    mainAdministratorsTable.buttons().container().appendTo('#mainAdministrators_wrapper .col-md-6:eq(0)');
+    pwdAdministratorsTable.buttons().container().appendTo('#pwdAdministrators_wrapper .col-md-6:eq(0)');
+    soloparentAdministratorsTable.buttons().container().appendTo('#soloparentAdministrators_wrapper .col-md-6:eq(0)');
+    srCitizenAdministratorsTable.buttons().container().appendTo('#srCitizenAdministrators_wrapper .col-md-6:eq(0)');
+    pwdDataTable.buttons().container().appendTo('#pwdDataTable_wrapper .col-md-6:eq(0)');
+    spDataTable.buttons().container().appendTo('#spDataTable_wrapper .col-md-6:eq(0)');
+    scDataTable.buttons().container().appendTo('#scDataTable_wrapper .col-md-6:eq(0)');
+    announcementsTable.buttons().container().appendTo('#announcements_wrapper .col-md-6:eq(0)');
+    
 });
 
 // Profile Picture
