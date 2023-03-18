74172499c4d54cf9ae1f072249847ba6a94004af
Romeo Jr Montealegre Bardillon <107736688+rmbardillon@users.noreply.github.com>
2023-03-18 14:20:03 +0800

    empty db

--- sql/new_cswd.sql
+++ sql/new_cswd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 16, 2023 at 04:04 PM
+-- Generation Time: Mar 18, 2023 at 07:00 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
